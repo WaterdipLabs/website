aca8dc1dc2715243bb987087be41e22de162d25f
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2022-12-25 20:13:16 +0530

    [Niyas][WD-241273] Minor Website Changes -1

--- src/pages/FeaturePage/FeaturePage.tsx
+++ src/pages/FeaturePage/FeaturePage.tsx
@@ -51,7 +51,6 @@ export default function FeaturePage(props) {
                     {
                         breakpoint: 850,
                         cols: 1,
-                        
                     }
                 ]}
                     mobileBreakpoint={1}>
