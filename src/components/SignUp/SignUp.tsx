26dfb93f8c61dbfc9d3b80f5404ce286d4ccf8fb
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2022-12-27 09:46:43 +0530

    [Niyas][WD-241273] Minor Website Changes - 2

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -3,7 +3,7 @@ import './signup.css';
 export default function SignUp(props): JSX.Element{
     return(
         <div className="signup">
-            <h1>Get a roundup with product updates, events, and the best blogs every few weeks.</h1>
+            <h1>Sign up for product updates: Get new release, product updates. No SPAM</h1>
             <div className="signup-signup">
                 <input type="text" placeholder="Full Name"/>
                 <input type="text" placeholder="Email"/>
