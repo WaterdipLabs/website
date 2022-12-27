164901d4776cffbe70e2245046cc2d53fa69da08
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2022-12-27 12:08:10 +0530

    [Niyas][WD-241273] Minor Website Changes - Newsletter Subscription + Footer

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,14 +1,74 @@
 import React from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 import './signup.css';
+
+const url = "https://waterdip.us1.list-manage.com/subscribe/post?u=3d60cb63df5a7669b5ba429f2&amp;id=878a0f23b6&amp;f_id=003ec9e5f0%22";
+const CustomForm = ({ status, message, onValidated }) => {
+    let email, name;
+    
+    const submit = () =>
+      email &&
+      name &&
+      email.value.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email.value,
+        NAME: name.value
+      });
+  
+    return (
+        <>
+      <div
+        className='signup-signup'
+      > 
+        <input
+          ref={node => (name = node)}
+          type="text"
+          placeholder="Full Name"
+        />
+        <br />
+        <input
+          ref={node => (email = node)}
+          type="email"
+          placeholder="Email"
+        />
+        <br />
+        <button onClick={submit}>
+          Sign Up
+        </button>
+        
+      </div>
+      {status === "sending" && <div className="sending">Sending...</div>}
+        {status === "error" && (
+          <div
+            className="sending"
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div
+            className="sending"
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}  
+      </>
+    );
+  };
+
+  
 export default function SignUp(props): JSX.Element{
     return(
         <div className="signup">
             <h1>Sign up for product updates: Get new release, product updates. No SPAM</h1>
-            <div className="signup-signup">
-                <input type="text" placeholder="Full Name"/>
-                <input type="text" placeholder="Email"/>
-                <button>Sign Up</button>
-            </div>
+            <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                <CustomForm
+                    status={status}
+                    message={message}
+                    onValidated={formData => subscribe(formData)}
+                />
+            )}
+            />
         </div>
     )
 }
