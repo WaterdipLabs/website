3be0c667dbb577602f1364947d908e87ebed932f
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-02-02 23:29:37 +0530

    [Niyas] Possible GitHub Actions Fix

--- src/components/Fcard/FCard.tsx
+++ src/components/Fcard/FCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './fcard.css';
 import clsx from "clsx";
 
-export default function BCard(props): JSX.Element {
+export default function FCard(props): JSX.Element {
     return(
         <div className="fcard">
             <img src={props.img} alt="desc" />
